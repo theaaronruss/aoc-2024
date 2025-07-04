a054d4cb89e3c3edc3ef145040abcb2208ea9dfb
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-07-04 16:47:40 -0400

    complete day 3 part 2

--- day3/main.go
+++ day3/main.go
@@ -17,6 +17,15 @@ func findInstructions(input string) []string {
 	return  matches
 }
 
+func findInstructionsWithConditionals(input string) []string {
+	regex := regexp.MustCompile(`mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)`)
+	matches := regex.FindAllString(input, -1)
+	if matches == nil {
+		return []string{}
+	}
+	return  matches
+}
+
 func parseInstructions(instructions []string) int {
 	regex := regexp.MustCompile("[0-9]{1,3}")
 	sum := 0
@@ -29,11 +38,37 @@ func parseInstructions(instructions []string) int {
 	return sum
 }
 
+func parseInstructionsWithConditionals(instructions []string) int {
+	valueRegex := regexp.MustCompile("[0-9]{1,3}")
+	sum := 0
+	do := true
+	for _, instruction := range instructions {
+		if instruction == "do()" {
+			do = true
+			continue
+		} else if instruction == "don't()" {
+			do = false
+			continue
+		}
+		if !do {
+			continue
+		}
+		values := valueRegex.FindAllString(instruction, -1)
+		num1, _ := strconv.Atoi(values[0])
+		num2, _ := strconv.Atoi(values[1])
+		sum += num1 * num2
+	}
+	return sum
+}
+
 func main() {
 	file, _ := os.Open("input.txt")
 	fileBytes, _ := io.ReadAll(file)
 	memory := string(fileBytes)
 	instructions := findInstructions(memory)
+	instructionsWithConditionals := findInstructionsWithConditionals(memory)
 	result := parseInstructions(instructions)
+	resultWithConditionals := parseInstructionsWithConditionals(instructionsWithConditionals)
 	fmt.Println("Result:", result)
+	fmt.Println("Result with conditionals:", resultWithConditionals)
 }
--- day3/main_test.go
+++ day3/main_test.go
@@ -20,6 +20,26 @@ func TestFindInstructions(t *testing.T) {
 	}
 }
 
+func TestFindInstructionsWithConditionals(t *testing.T) {
+	matches := findInstructionsWithConditionals("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+	expected := []string {
+		"mul(2,4)",
+		"don't()",
+		"mul(5,5)",
+		"mul(11,8)",
+		"do()",
+		"mul(8,5)",
+	}
+	if len(matches) != len(expected) {
+		t.Fatal("Incorrect number of matches")
+	}
+	for i, match := range matches {
+		if match != expected[i] {
+			t.Fatal("Incorrect match")
+		}
+	}
+}
+
 func TestParseInstruction(t *testing.T) {
 	instructions := []string {
 		"mul(2,4)",
@@ -29,6 +49,22 @@ func TestParseInstruction(t *testing.T) {
 	}
 	result := parseInstructions(instructions)
 	expected := 161
+	if result != expected {
+		t.Fatalf("Result was %d, not %d", result, expected)
+	}
+}
+
+func TestParseInstructionsWithConditionals(t *testing.T) {
+	instructions := []string {
+		"mul(2,4)",
+		"don't()",
+		"mul(5,5)",
+		"mul(11,8)",
+		"do()",
+		"mul(8,5)",
+	}
+	result := parseInstructionsWithConditionals(instructions)
+	expected := 48
 	if result != expected {
 		t.Fatalf("Result was %d, not %d", result, expected)
 	}
