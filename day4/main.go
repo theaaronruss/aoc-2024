bb45ea9acfae5dee614d303953f8dbff2c423713
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-07-05 01:49:43 -0400

    complete day 4 part 2

--- day4/main.go
+++ day4/main.go
@@ -6,7 +6,8 @@ import (
 	"os"
 )
 
-func findOccurrences(puzzle []string, word string) int {
+func findOccurrences(puzzle []string) int {
+	word := "XMAS"
 	occurrences := 0
 	for y := range puzzle {
 		for x, char := range puzzle[y] {
@@ -42,6 +43,18 @@ func findOccurrences(puzzle []string, word string) int {
 	return occurrences
 }
 
+func findOccurrencesX(puzzle []string) int {
+	occurrences := 0
+	for y := range puzzle {
+		for x, char := range puzzle[y] {
+			if byte(char) == 'A' && isMatchX(puzzle, x, y) {
+				occurrences++
+			}
+		}
+	}
+	return occurrences
+}
+
 func isMatch(puzzle []string, x int, y int, xDir int, yDir int, word string, index int) bool {
 	if index >= len(word) {
 		return true
@@ -55,6 +68,19 @@ func isMatch(puzzle []string, x int, y int, xDir int, yDir int, word string, ind
 	return isMatch(puzzle, x + xDir, y + yDir, xDir, yDir, word, index + 1)
 }
 
+func isMatchX(puzzle []string, x, y int) bool {
+	if x < 1 || x >= len(puzzle[0]) - 1 || y < 1 || y >= len(puzzle) - 1 {
+		return false
+	}
+	if (puzzle[y - 1][x - 1] == 'M' && puzzle[y + 1][x + 1] == 'S' ||
+		puzzle[y - 1][x - 1] == 'S' && puzzle[y + 1][x + 1] == 'M') &&
+		(puzzle[y - 1][x + 1] == 'M' && puzzle[y + 1][x - 1] == 'S' ||
+		puzzle[y - 1][x + 1] == 'S' && puzzle[y + 1][x - 1] == 'M') {
+			return true
+	}
+	return false
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		panic("Invalid arguments")
@@ -65,6 +91,8 @@ func main() {
 	for scanner.Scan() {
 		puzzle = append(puzzle, scanner.Text())
 	}
-	occurrences := findOccurrences(puzzle, "XMAS")
+	occurrences := findOccurrences(puzzle)
 	fmt.Println("Occurrences:", occurrences)
+	occurrencesX := findOccurrencesX(puzzle)
+	fmt.Println("Occurrences with X:", occurrencesX)
 }
